4582ce793ce83dcd740750c05c00663c31622bd9
Evan <evanslin@gmail.com>
2020-05-14 01:36:08 +0800

    add emoji

--- pet.go
+++ pet.go
@@ -15,14 +15,10 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"golang.org/x/text/encoding/unicode/utf32"
 )
 
 //PetType :
 type PetType int
-
-var BrownEmoji string
 
 const (
 	//Dog :
@@ -30,17 +26,6 @@ const (
 	//Cat :
 	Cat
 )
-
-func init() {
-	var err error
-	utf32BEIB := utf32.UTF32(utf32.BigEndian, utf32.IgnoreBOM)
-	dec := utf32BEIB.NewDecoder()
-	BrownEmoji, err = dec.String("\x00\x10\x00\x84")
-	if err != nil {
-		log.Print(err)
-	}
-
-}
 
 //Pet :
 type Pet struct {
@@ -86,5 +71,5 @@ func (p *Pet) DisplayPet() string {
 		log.Println("img:", p.ImageName)
 	}
 
-	return fmt.Sprintf("%s 您好 \n 動物品種: %s \n 毛色: %s \n 體型: %s \n 性別: %s \n 名為: %s \n 公告收容所: %s \n 領養電話為: %s  \n 圖片位置: %s", BrownEmoji, p.Variety, p.HairType, p.Type, p.Sex, p.Name, p.Resettlement, p.Phone, p.ImageName)
+	return fmt.Sprintf("$ 您好 \n 動物品種: %s \n 毛色: %s \n 體型: %s \n 性別: %s \n 名為: %s \n 公告收容所: %s \n 領養電話為: %s  \n 圖片位置: %s", p.Variety, p.HairType, p.Type, p.Sex, p.Name, p.Resettlement, p.Phone, p.ImageName)
 }
